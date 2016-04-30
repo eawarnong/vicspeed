160218f5deb4ab078d9d6565fad45e7d6cf3965d
eawarnong <57130500219@st.sit.kmutt.ac.th>
2016-04-30 15:47:22 +0700

    SO and invoice can get all info of products

--- database/SaleOrder.java
+++ database/SaleOrder.java
@@ -81,6 +81,24 @@ public class SaleOrder extends ConnectDB {
         ArrayList<HashMap> proid = db.queryRows(sql);
         return db.queryRows(sql);
     }
+    
+    public ArrayList<HashMap> getProducts() {
+        ArrayList<HashMap> proids = getProductsID();
+        ArrayList<HashMap> products = new ArrayList<HashMap>();
+        for(HashMap proid : proids) {
+            Product product = new Product();
+            HashMap productHashMap = new HashMap();
+            //product.setProductID(Integer.parseInt(String.valueOf(proid.get("PRODUCTID"))));
+            productHashMap.put("PRODUCTID", product.getProductID());
+            productHashMap.put("PRODUCTNAME", product.getProductName());
+            productHashMap.put("TYPEOFPRODUCT", product.getProductType());
+            productHashMap.put("AMOUNT", product.getProductAmount());
+            productHashMap.put("PRICE", product.getProductPrice());
+            productHashMap.put("TOTALPRICE", product.getTotalPrice());
+            products.add(productHashMap);
+        }
+        return products;
+    }
 
 }
 
